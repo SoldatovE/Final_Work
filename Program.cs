3d6a75596730f418761767613ff4b2a2abea4692
Egor <egoos_18@mail.ru>
2024-02-21 19:08:55 +0300

    Начало создания и редактирования программы.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
+
+using System;
+
+class Program
+{static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строки через запятую:");
+        string input = Console.ReadLine();
+        string[] inputStrings = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        
+        string[] result = FilterShortStrings(inputStrings);
+
+        Console.WriteLine("Результат:");
+        foreach (string str in result)
+        {
+            Console.WriteLine(str);
+        }
+    }
+    
+}
